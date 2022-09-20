6eff66edc7ef2cafa5c67f58b082be9043b1113f
KINGZMON <myf11302@gmail.com>
2022-09-20 18:33:42 +0300

    2nd commit

--- app/src/main/java/com/example/worldhello/MainActivity.java
+++ app/src/main/java/com/example/worldhello/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.worldhello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        TextView text = findViewById(R.id.textView);
+        text.setText("I've been expecting you Mr.Martas");
     }
 }
