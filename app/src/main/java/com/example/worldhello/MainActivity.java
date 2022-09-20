049106b85a81357ca66fb2b4524b218fcce7724a
KINGZMON <myf11302@gmail.com>
2022-09-20 20:28:26 +0300

    Revert "3rd commit"
    This reverts commit b424fa228dc4341adba8509b013959435a52be7d.

--- app/src/main/java/com/example/worldhello/MainActivity.java
+++ app/src/main/java/com/example/worldhello/MainActivity.java
@@ -2,13 +2,10 @@ package com.example.worldhello;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View v1) {
+    public void onClick(View view) {
         TextView text = findViewById(R.id.textView);
         text.setText("I've been expecting you Mr.Martas");
-    }
-
-    public void onClick2(View v2) {
-        TextView text = findViewById(R.id.textView);
-        Random random = new Random();
-        int color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256));
-        text.setTextColor(color);
     }
 }
